2bad4a96043f8e26b31fe04d21b51523a7981c76
Vladimir101 <vladimir100@hotmail.com>
2019-02-07 22:11:42 -0800

    Hamcrest

---
+++ JUnit4Examples/src/test/java/com/junit4/examples/HamcrestAssertions.java
@@ -0,0 +1,31 @@
+// require hamcrest-all: 1.3
+package com.junit4.examples;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.hamcrest.Matchers.*;
+
+public class HamcrestAssertions
+{
+	@Test
+	public void test()
+	{
+		int sum = 2;
+		assertThat("sum test", sum, equalTo(2));
+		assertThat(sum, is(2));
+		assertThat(sum, is(not(3)));
+		assertThat(sum, greaterThan(0));
+		assertThat(sum, is(greaterThan(1)));
+		
+		String str = "White-box testing";
+		assertThat(str, containsString("White"));
+		assertThat(str, startsWith("White"));
+		assertThat(str, endsWith("testing"));
+		assertThat(str, equalToIgnoringCase("white-box testing"));
+		assertThat(str, not(equalToIgnoringCase("black-box testing")));
+		
+		String[] cars = {"Ferrari", "Acura", "Lamborgini"};
+		String[] myCars = {"Ferrari", "Acura", "Lamborgini"};
+		assertArrayEquals(cars, myCars);
+	}
+}
